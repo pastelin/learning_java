090a8d7dad309f2172484515c1c423d3d295e0d6
juan.pastelin <juan.pastelin@bbva.com>
2025-06-25 10:46:47 -0600

    Agrega declaracion for

---
+++ GlobalMentoring/tareas/src/main/java/gm/tareas/controlador/IndexControlador.java
@@ -0,0 +1,5 @@
+package gm.tareas.controlador;
+import javafx.fxml.Initializable;
+
+public class IndexControlador implements Initilizable {
+}
---
+++ TimBuchalka/01-CodingExercise/src/main/java/control/flow/statement_for/Main.java
@@ -0,0 +1,35 @@
+package control.flow.statement_for;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        for(int counter = 1; counter <= 10; counter++) {
+            System.out.println("Counter: " + counter);
+
+        }
+
+        for(double rate = 2.0; rate <= 5.0; rate++) {
+            double interestAmount = calculateInterest(10000.0, rate);
+            System.out.println("10,000 at " + rate + "% interest = " + interestAmount);
+        }
+
+        calculateInterest();
+
+
+    }
+
+    public static void calculateInterest() {
+
+        for (double rate = 7.5; rate <= 10.0; rate+=.25) {
+            System.out.println("100 at " + rate + "% interest = " + calculateInterest(100.0, rate));
+        }
+    }
+
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return amount * (interestRate / 100);
+    }
+
+}
